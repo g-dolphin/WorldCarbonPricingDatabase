de266ed29449939347953788fb8b796ce6798759
GDolphin <gd396@cam.ac.uk>
2021-08-17 13:27:39 +0100

    agg dummy script

--- scripts/agg_dum.py
+++ scripts/agg_dum.py
@@ -13,26 +13,8 @@ import numpy as np
 
 from collections import defaultdict
 
-ipcc_codes = pd.read_csv("/Users/gd/OneDrive - rff/Documents/Research/projects/ecp/wcpd_dataset/dataset_structure/IPCC2006_category_codes_links.csv")
-
-multivalue_dict = defaultdict(list)
-
-for idx,row in ipcc_codes.iterrows():
-    multivalue_dict[row['parent_category']].append(row['IPCC_CODE'])
-
 
 # Assign values to aggregate categories if values in all subcategories is identical (UNDER DEVELOPMENT)
-# Disaggregation levels
-
-# Extract disaggregation levels
-level_6 = [x for x in list(ipcc_codes.IPCC_CODE.unique()) if len(x) == 6]
-level_5 = [x for x in list(ipcc_codes.IPCC_CODE.unique()) if len(x) == 5]
-level_4 = [x for x in list(ipcc_codes.IPCC_CODE.unique()) if len(x) == 4]
-level_3 = [x for x in list(ipcc_codes.IPCC_CODE.unique()) if len(x) == 3]
-level_2 = [x for x in list(ipcc_codes.IPCC_CODE.unique()) if len(x) == 2]
-level_1 = [x for x in list(ipcc_codes.IPCC_CODE.unique()) if len(x) == 1]
-
-ipcc_code_levels = [level_5, level_4, level_3, level_2, level_1]
 
 def concatenate(indir):#,outfile):
     os.chdir(indir) #sets the current directory to 'indir'
@@ -71,26 +53,56 @@ all_jur["ETS_dummy"] = all_jur["ETS_dummy"].astype(int)
 all_jur_agg = all_jur.groupby(["Jurisdiction", "Year", "IPCC_cat_code"]).sum()
 all_jur_agg = all_jur_agg.reset_index()
 
+# Replace all '3' values (which indicate that all fuels are covered) with '1'
+# and all other values with '0'
+
 all_jur_agg["Tax_dummy"] = np.where(all_jur_agg.Tax_dummy==3,1,0)
 all_jur_agg["ETS_dummy"] = np.where(all_jur_agg.ETS_dummy==3,1,0)
 
-# Dummy calculation
+# Aggregate sectors dummy calculation
+
+# IPCC code levels
+ipcc_codes = pd.read_csv("/Users/gd/OneDrive - rff/Documents/Research/projects/ecp/wcpd_dataset/dataset_aux_files/other/IPCC2006_category_codes_links.csv")
+
+ipcc_sec_subsec_dict = defaultdict(list)
+
+for idx,row in ipcc_codes.iterrows():
+    ipcc_sec_subsec_dict[row['parent_category']].append(row['IPCC_CODE'])
+
+
+# Extract disaggregation levels
+level_6 = [x for x in list(ipcc_codes.IPCC_CODE.unique()) if len(x) == 6]
+level_5 = [x for x in list(ipcc_codes.IPCC_CODE.unique()) if len(x) == 5]
+level_4 = [x for x in list(ipcc_codes.IPCC_CODE.unique()) if len(x) == 4]
+level_3 = [x for x in list(ipcc_codes.IPCC_CODE.unique()) if len(x) == 3]
+level_2 = [x for x in list(ipcc_codes.IPCC_CODE.unique()) if len(x) == 2]
+level_1 = [x for x in list(ipcc_codes.IPCC_CODE.unique()) if len(x) == 1]
+
+ipcc_code_levels = [level_5, level_4, level_3, level_2, level_1]
 
 # For each level, check whether all values for lower disaggregation level are equal to 1
-for jur in all_jur.Jurisdiction.unique():
-    for yr in all_jur.Year.unique():
-        for level in ipcc_code_levels:
+for jur in ["Sweden"]:#all_jur.Jurisdiction.unique():
+    for yr in [2010]:#all_jur.Year.unique():
+        for level in [level_5]:#ipcc_code_levels:
             for ipcc_code in level:
-                x_tax = 1
-                x_ets = 1
-                
-                for ipcc_code_1 in multivalue_dict[ipcc_code]:
-                    x_tax *=  all_jur_agg.loc[(all_jur_agg.Jurisdiction==jur) & (all_jur_agg.Year==yr) & (all_jur_agg.IPCC_cat_code==ipcc_code_1), "Tax_dummy"].item()
-                    x_ets *=  all_jur_agg.loc[(all_jur_agg.Jurisdiction==jur) & (all_jur_agg.Year==yr) & (all_jur_agg.IPCC_cat_code==ipcc_code_1), "ETS_dummy"].item()
+
+                if len(ipcc_sec_subsec_dict[ipcc_code]) != 0: #check the list is not empty
+                    x_tax = 1
+                    x_ets = 1                
                     
-                all_jur.loc[(all_jur.Jurisdiction==jur) & (all_jur.Year==yr) & (all_jur.IPCC_cat_code==ipcc_code), "Tax_dummy"] = x_tax
-                all_jur.loc[(all_jur.Jurisdiction==jur) & (all_jur.Year==yr) & (all_jur.IPCC_cat_code==ipcc_code), "ETS_dummy"] = x_ets
-            
+                    for ipcc_code_1 in ipcc_sec_subsec_dict[ipcc_code]:
+                        x_tax *=  all_jur_agg.loc[(all_jur_agg.Jurisdiction==jur) & (all_jur_agg.Year==yr) & (all_jur_agg.IPCC_cat_code==ipcc_code_1), "Tax_dummy"].item()
+                        x_ets *=  all_jur_agg.loc[(all_jur_agg.Jurisdiction==jur) & (all_jur_agg.Year==yr) & (all_jur_agg.IPCC_cat_code==ipcc_code_1), "ETS_dummy"].item()
+                                        
+                else:
+                    x_tax = all_jur_agg.loc[(all_jur_agg.Jurisdiction==jur) & (all_jur_agg.Year==yr) & (all_jur_agg.IPCC_cat_code==ipcc_code), "Tax_dummy"].item()
+                    x_ets = all_jur_agg.loc[(all_jur_agg.Jurisdiction==jur) & (all_jur_agg.Year==yr) & (all_jur_agg.IPCC_cat_code==ipcc_code), "ETS_dummy"].item()
+
+                #all_jur.loc[(all_jur.Jurisdiction==jur) & (all_jur.Year==yr) & (all_jur.IPCC_cat_code==ipcc_code), "Tax_dummy"] = x_tax
+                #all_jur.loc[(all_jur.Jurisdiction==jur) & (all_jur.Year==yr) & (all_jur.IPCC_cat_code==ipcc_code), "ETS_dummy"] = x_ets
+
+
+                print(ipcc_code, "tax",x_tax, "ets", x_ets)
             
             
             
