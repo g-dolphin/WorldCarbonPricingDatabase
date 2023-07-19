6e7d8c5f99c29012ca29b982967122c89a4912b6
GDolphin <gd396@cam.ac.uk>
2023-07-18 19:58:28 -0700

    scope update cont.

--- _raw/scope/ets/ets_scope_CO2.py
+++ _raw/scope/ets/ets_scope_CO2.py
@@ -139,7 +139,7 @@ def scope():
     
     aut_ets_jur_scope = {2022:aut_ets_jur_I}
     
-    aut_ets_ipcc_scope = {2022:aut_ets_jur_I}
+    aut_ets_ipcc_scope = {2022:aut_ets_ipcc_I}
     
     ## Sources dictionary
     
@@ -686,7 +686,9 @@ def scope():
 
     usa_or_ets_jur_I = ["Oregon"]
 
-    usa_or_ets_ipcc_I = []
+    usa_or_ets_ipcc_I = ["1A1A1", "1A1A2", "1A1B", "1A1C", "1A2A", "1A2B", "1A2C",
+                         "1A2D", "1A2E", "1A2F", "1A2G", "1A2H", "1A2I", "1A2J", "1A2K",
+                         "1A2L", "1A2M", "1A3B", "1A4A", "1A4B", "1A5A"]
 
     usa_or_ets_jur_scope = {2020:usa_or_ets_jur_I,
                             2021:usa_or_ets_jur_I,
@@ -730,7 +732,16 @@ def scope():
 
     usa_wa_ets_jur_I = ["Washington"]
 
-    usa_wa_ets_ipcc_I = []
+    usa_wa_ets_ipcc_I = ["1A1A1", "1A1A2", "1A1B", "1A1C", "1A2A", "1A2B", "1A2C",
+                        "1A2D", "1A2E", "1A2F", "1A2G", "1A2H", "1A2I", "1A2J", "1A2K",
+                        "1A2L", "1A2M", "1A5A", "1C1A", "1C1B",
+                        "2A1", "2A2", "2A3", "2C1", "2C5", "2H1",  
+                        "2A4", "2A4A", "2A4B", "2A4C", "2A4D", "2B1", "2B10", "2B2", 
+                        "2B3", "2B4", "2B5", "2B6", "2B7", "2B8", "2B8A", "2B8B", 
+                        "2B8C", "2B8D", "2B8E", "2B8F", "2B9A", "2B9B", "2C1", "2C2", 
+                        "2C3", "2C4", "2C5", "2C6", "2C7", "2D1", "2D2","2D3", "2D4", 
+                        "2E", "2F1", "2F2", "2F3", "2F4", "2F5", "2F6", "2G1", "2G2", 
+                        "2G3", "2G4", "2H1", "2H2", "2H3"]
 
     usa_wa_ets_jur_scope = {2023:usa_wa_ets_jur_I}
     
