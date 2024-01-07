3c14e20289d5cedc58e5abdd65cd580c5bae3434
GDolphin <gd396@cam.ac.uk>
2024-01-06 23:44:00 -0500

    price updates

--- _code/_compilation/db_build.py
+++ _code/_compilation/db_build.py
@@ -214,7 +214,7 @@ wcpd_all_jur.loc[wcpd_all_jur.ets!=1, "ets"] = 0
 # Price-based exemptions
 # Add (price-based) exemptions/rebate column for carbon taxes
 
-stream = open("_raw/priceRebates/tax/_price_exemptions_tax_"+gas+".py")
+stream = open("/Users/gd/GitHub/WorldCarbonPricingDatabase/_raw/priceRebates/tax/_price_exemptions_tax_"+gas+".py")
 read_file = stream.read()
 exec(read_file)
 
