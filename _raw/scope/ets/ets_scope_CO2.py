9109c99f5758929b35bfa80f6fb949ce866b6c8c
GDolphin <gd396@cam.ac.uk>
2024-10-12 16:14:46 -0400

    update to scope file, simplification of dictionaries (using loops)

--- _raw/scope/ets/ets_scope_CO2.py
+++ _raw/scope/ets/ets_scope_CO2.py
@@ -306,24 +306,11 @@ def scope():
     
     ## scope dictionaries
     
-    nzl_ets_jur_scope = {2008:nzl_ets_jur_I,
-                         2009:nzl_ets_jur_I, 2010:nzl_ets_jur_I,
-                         2011:nzl_ets_jur_I, 2012:nzl_ets_jur_I,
-                         2013:nzl_ets_jur_I, 2014:nzl_ets_jur_I,
-                         2015:nzl_ets_jur_I, 2016:nzl_ets_jur_I,
-                         2017:nzl_ets_jur_I, 2018:nzl_ets_jur_I,
-                         2019:nzl_ets_jur_I, 2020:nzl_ets_jur_I,
-                         2021:nzl_ets_jur_I, 2022:nzl_ets_jur_I}
+    nzl_ets_jur_scope = {year: nzl_ets_jur_I for year in range(2008, 2023)}
     
-    nzl_ets_ipcc_scope = {2008:nzl_ets_ipcc_I,
-                          2009:nzl_ets_ipcc_I, 2010:nzl_ets_ipcc_II,
-                          2011:nzl_ets_ipcc_II, 2012:nzl_ets_ipcc_II,
-                          2013:nzl_ets_ipcc_III, 2014:nzl_ets_ipcc_III,
-                          2015:nzl_ets_ipcc_III, 2016:nzl_ets_ipcc_III,
-                          2017:nzl_ets_ipcc_III, 2018:nzl_ets_ipcc_III,
-                          2019:nzl_ets_ipcc_III, 2020:nzl_ets_ipcc_III,
-                          2021:nzl_ets_ipcc_III, 2022:nzl_ets_ipcc_III}
-    
+    nzl_ets_ipcc_scope = {year: nzl_ets_ipcc_I for year in range(2008, 2010)}
+    nzl_ets_ipcc_scope.update({year: nzl_ets_ipcc_II for year in range(2010, 2013)})
+    nzl_ets_ipcc_scope.update({year: nzl_ets_ipcc_III for year in range(2013, 2023)})    
 
     ## Sources dictionary
     
@@ -362,23 +349,10 @@ def scope():
                       "2C5", "2C6", "2C7", "2H1"]
     
     ## scope dictionaries
-    che_ets_jur_scope = {2008:che_ets_jur_I,
-                            2009:che_ets_jur_I, 2010:che_ets_jur_I,
-                            2011:che_ets_jur_I, 2012:che_ets_jur_I,
-                            2013:che_ets_jur_I, 2014:che_ets_jur_I,
-                            2015:che_ets_jur_I, 2016:che_ets_jur_I,
-                            2017:che_ets_jur_I, 2018:che_ets_jur_I,
-                            2019:che_ets_jur_I, 2020:che_ets_jur_I,
-                            2021:che_ets_jur_I, 2022:che_ets_jur_I}
+    che_ets_jur_scope = {year: che_ets_jur_I for year in range(2008, 2023)}
     
-    che_ets_ipcc_scope = {2008:che_ets_ipcc_I,
-                            2009:che_ets_ipcc_I, 2010:che_ets_ipcc_I,
-                            2011:che_ets_ipcc_I, 2012:che_ets_ipcc_I,
-                            2013:che_ets_ipcc_I, 2014:che_ets_ipcc_I,
-                            2015:che_ets_ipcc_I, 2016:che_ets_ipcc_I,
-                            2017:che_ets_ipcc_I, 2018:che_ets_ipcc_I,
-                            2019:che_ets_ipcc_I, 2020:che_ets_ipcc_II,
-                            2021:che_ets_ipcc_II, 2022:che_ets_ipcc_II}
+    che_ets_ipcc_scope = {year: che_ets_ipcc_I for year in range(2008, 2020)}
+    che_ets_ipcc_scope.update({year: che_ets_ipcc_II for year in range(2020, 2023)})
     
     ## Sources dictionary
     
@@ -428,17 +402,11 @@ def scope():
                           "2C1", "2C2", "2C3", "2C4", "2C5", "2C6", "2C7"]
     
     ## scope dictionaries
-    kaz_ets_jur_scope = {2013:kaz_ets_jur_I, 2014:kaz_ets_jur_I,
-                            2015:kaz_ets_jur_I, 2016:kaz_ets_jur_I,
-                            2017:kaz_ets_jur_I, 2018:kaz_ets_jur_I,
-                            2019:kaz_ets_jur_I, 2020:kaz_ets_jur_I,
-                            2021:kaz_ets_jur_I, 2022:kaz_ets_jur_I}
+    kaz_ets_jur_scope = {year: kaz_ets_jur_I for year in range(2013, 2023)}
     
-    kaz_ets_ipcc_scope = {2013:kaz_ets_ipcc_I, 2014:kaz_ets_ipcc_I,
-                             2015:kaz_ets_ipcc_I, 2016:kaz_ets_ipcc_II,
-                             2017:kaz_ets_ipcc_II, 2018:kaz_ets_ipcc_III,
-                             2019:kaz_ets_ipcc_III, 2020:kaz_ets_ipcc_III,
-                             2021:kaz_ets_ipcc_III, 2022:kaz_ets_ipcc_III}
+    kaz_ets_ipcc_scope = {year: kaz_ets_ipcc_I for year in range(2013, 2016)}
+    kaz_ets_ipcc_scope.update({year: kaz_ets_ipcc_I for year in range(2016, 2018)})
+    kaz_ets_ipcc_scope.update({year: kaz_ets_ipcc_I for year in range(2018, 2023)})
     
     ## Sources dictionary
     
@@ -501,15 +469,11 @@ def scope():
                         "4A1", "4D1", "4D2"]
     
     ## scope dictionaries
-    kor_ets_jur_scope = {2015:kor_ets_jur_I, 2016:kor_ets_jur_I,
-                         2017:kor_ets_jur_I, 2018:kor_ets_jur_I,
-                         2019:kor_ets_jur_I, 2020:kor_ets_jur_I,
-                         2021:kor_ets_jur_I, 2022:kor_ets_jur_I}
+    kor_ets_jur_scope = {year: kor_ets_jur_I for year in range(2015, 2023)}
     
-    kor_ets_ipcc_scope = {2015:kor_ets_ipcc_I, 2016:kor_ets_ipcc_I,
-                          2017:kor_ets_ipcc_I, 2018:kor_ets_ipcc_II,
-                          2019:kor_ets_ipcc_II, 2020:kor_ets_ipcc_II,
-                          2021:kor_ets_ipcc_III, 2022:kor_ets_ipcc_III}
+    kor_ets_ipcc_scope = {year: kor_ets_ipcc_I for year in range(2015, 2018)}
+    kor_ets_ipcc_scope.update({year: kor_ets_ipcc_I for year in range(2018, 2021)})
+    kor_ets_ipcc_scope.update({year: kor_ets_ipcc_I for year in range(2021, 2023)})
     
     ## Sources dictionary
     
@@ -604,13 +568,7 @@ def scope():
                             2019:usa_rggi_jur_I, 2020:usa_rggi_jur_I,
                             2021:usa_rggi_jur_I, 2022:usa_rggi_jur_I}
     
-    usa_rggi_ipcc_scope = { 2009:usa_rggi_ipcc_I, 2010:usa_rggi_ipcc_I,
-                            2011:usa_rggi_ipcc_I, 2012:usa_rggi_ipcc_I,
-                            2013:usa_rggi_ipcc_I, 2014:usa_rggi_ipcc_I,
-                            2015:usa_rggi_ipcc_I, 2016:usa_rggi_ipcc_I,
-                            2017:usa_rggi_ipcc_I, 2018:usa_rggi_ipcc_I,
-                            2019:usa_rggi_ipcc_I, 2020:usa_rggi_ipcc_I,
-                            2021:usa_rggi_ipcc_I, 2022:usa_rggi_ipcc_I}
+    usa_rggi_ipcc_scope = {year: usa_rggi_ipcc_I for year in range(2009, 2023)}
     
     ## Sources dictionary
     
