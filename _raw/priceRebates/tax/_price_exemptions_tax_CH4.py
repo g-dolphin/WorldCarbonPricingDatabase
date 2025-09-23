42a0d069a03809f5775f77534843be94c500b07f
GDolphin <gd396@cam.ac.uk>
2025-09-23 11:02:57 -0400

    price exemptions updates CO2/CH4/N2O

---
+++ _raw/priceRebates/tax/_price_exemptions_tax_CH4.py
@@ -0,0 +1,11 @@
+# This script creates the files recording the tax exemption/rebate data
+# Current records have been manually constructed by encoding entries in raw csv files.
+# The next update of this file will code these exemptions in the dedicated section below
+
+#--------------------------------Exemption/rebate coding-----------------------------
+
+#-------------------------------------------------------------------------------------
+
+tax_exemptions = []
+
+tax_exemptions_sources = []
